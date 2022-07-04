db08ebd95073ad82591f6553501efdd23c0a806c
laurazig <lhzig@protonmail.com>
2022-07-04 10:08:23 +0200

    delete one working. no pop up delte all- log out

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,8 +1,5 @@
-import { v4 as uuid } from "uuid";
 import User from "../models/user.js";
 import createError from "http-errors";
-
-
 
 // ==============================================
 // GET the logged in user's data
@@ -17,9 +14,10 @@ export const getUserData = async (req, res, next) => {
     //const foundUser = db.data.users.find(user => user.id === userId);
     let foundUser;
     try {
-        foundUser = await User.findOne({_id:userId})
+        // foundUser = await User.findOne({_id:userId})
+        foundUser = await User.findById(userId);
     } catch {
-        return next(createError(500, "user could not be create(hello http-errors!). Please try again"));
+        return next(createError(500, "findById -user could not be created (http-errors in userController)"));
     }
 
     // If a user was found with the same id as the :id parameter...
@@ -31,13 +29,13 @@ export const getUserData = async (req, res, next) => {
             firstName: foundUser.firstName,
             albums: foundUser.albums
         }
-
         res.json(userData);
     
     // If no user was found with the same id as the :id parameter...
     // Create an error object with a relevant message and statusCode, and pass it to the error handling middleware
     } else {
-        next(new createError.InternalServerError("user could not be created. Please try again"))
+        // next(new createError.InternalServerError("user could not be created. Please try again"))
+        next(createError(404, "user could not be created. Please try again"))
     }
 }
 
@@ -46,21 +44,67 @@ export const getUserData = async (req, res, next) => {
 // =======================================================
 
 export const postAlbum = async (req, res, next) => {
-    const { band, albumTitle, albumYear } = req.body;
+    
+     // * Step 1: Grab the album data from the request body
+    const newAlbum=req.body //this is new code but where is body defined??
+     // Take the user's id from the "id" parameter of their request URL
+     const userId = req.params.id;
 
-    const newAlbums = {
-       // id: uuid(),
-        band: band,
-        albumTitle: albumTitle,
-        albumYear: albumYear
-    }
+     // * Step 2A - Find the user who sent the request in our "users" collection.
+     let foundUser; 
+     
+     try {
+         foundUser = await User.findById(userId);
+     } catch {
+         return next(createError(500, "Query could not be completed. Please try again"));
+     }
+ 
+     // * Step 2B - Search in the user's array of albums to see if they already have the new album there
+     // If foundAlbum = undefined, the new album is not already in their "albums" array - we can create it!
+     const foundAlbum = foundUser.albums.find(album => {
+         return album.band.toLowerCase() === newAlbum.band.toLowerCase()
+             && album.albumTitle.toLowerCase() === newAlbum.albumTitle.toLowerCase()
+             && album.albumYear == newAlbum.albumYear
+     })
+ 
+     // * Step 3A - If the user does not already have the new album in their "albums" array...
+     if (!foundAlbum) {
+         let updatedUser;
+ 
+         try {
+             //                                        (1) id  (2) update                      (3) options
+             updatedUser = await User.findByIdAndUpdate(userId, { $push: { albums: newAlbum }}, { new: true, runValidators: true })
+         } catch {
+             return next(createError(500, "User could not be updated. Please try again"));
+         }
+ 
+         // * ... and send back the updated array of albums to the frontend
+         res.status(201).json(updatedUser.albums);
+     
+     // If the new album is already in the user's "albums" array...
+     // Create an error object with a relevant message and statusCode, and pass it to the error handling middleware    
+     } else {
+         next(createError(409, "The album already exists in your collection!"));
+     }
+ }
 
-    // Take the user's id from the "id" parameter of their request URL
-    const userId = req.params.id;
+//=========================2nd version==================================
+    // const { band, albumTitle, albumYear } = req.body;
 
+    // const newAlbums = {
+    //    // id: uuid(),
+    //     band: band,
+    //     albumTitle: albumTitle,
+    //     albumYear: albumYear
+    // }
+    // const userId = req.params.id;
+    // const newUser= await User.findByIdAndUpdate(userId, {$push:{albums:newAlbums}},{new:true,runValidators:true})
+    // const allAlbums=newUser.albums
+    // res.status(201).json(allAlbums);
+// ==========================lowdb version===============================================
+ 
     // Find the index of the user inside the "users" array of the database file
    // const indexOfUser = db.data.users.findIndex(user => user.id === userId);
-
 
     // Search in the user's array of albums to see if they already have the new album there
     // const foundAlbum = db.data.users[indexOfUser].albums.find(album => {
@@ -68,15 +112,14 @@ export const postAlbum = async (req, res, next) => {
     //         && album.albumTitle.toLowerCase() === albumTitle.toLowerCase() 
     //         && album.albumYear === albumYear
     // })
-    const newUser= await User.findByIdAndUpdate(userId, {$push:{albums:newAlbums}},{new:true,runValidators:true})
+
     // If the user does not already have the new album in their "albums" array...
     // if (!foundAlbum) {    // ]    //     })
     //    // db.data.users[indexOfUser].albums.push(newAlbum);
     //     //await db.write();
     //     await newAlbum.save();
     //     const allAlbums = await User.findIndex().albums
-    const allAlbums=newUser.albums
-        res.status(201).json(allAlbums);
+   
     // If the new album is already in the user's "albums" array...
     // Create an error object with a relevant message and statusCode, and pass it to the error handling middleware    
     // } else {
@@ -85,7 +128,7 @@ export const postAlbum = async (req, res, next) => {
     //     // next(err);
     //     return next(createError(409, "The album already exists in your collection!"));
     // }
-}
+
 
 // =======================================================
 // DELETE all albums from the logged in user's "albums" list  02.06.22
@@ -93,7 +136,7 @@ export const postAlbum = async (req, res, next) => {
 
 export const deleteAlbums = async (req, res, next) => {
     const userId = req.params.id;
-
+   // Find the user who sent the request. update their "albums" array to be an empty array
     let updatedUser;
     try {
         updatedUser = await User.findByIdAndUpdate(userId, { albums:[] }, { new: true, runValidators:true})
@@ -126,3 +169,26 @@ export const deleteAlbums = async (req, res, next) => {
 //         err.statusCode = 404;
 //         next(err);
 //     }
+
+// =============================================================
+// DELETE a single album from the logged in user's "albums" list     02.06.22
+// =============================================================
+
+export const deleteAlbum = async (req, res, next) => {
+    const userId = req.params.id;
+    const albumId = req.params.albumId;
+
+    let updatedUser;
+
+    try {
+        // (1) "Pull something (remove it)..."
+        // (2) "From the user's 'albums' array"
+        // (3) "The thing to pull out of the array is the one with an _id property === albumId"
+        //                                                    (1)      (2)          (3)
+        updatedUser = await User.findByIdAndUpdate(userId, { $pull: { albums: { _id: albumId } }}, { new: true, runValidators: true })
+    } catch {
+        return next(createError(500, "The user could not be updated. DeleteAlbum not successful."));
+    }
+
+    res.json(updatedUser.albums);
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserData, postAlbum, deleteAlbums } from "../controllers/usersController.js";
+import { getUserData, postAlbum, deleteAlbums, deleteAlbum } from "../controllers/usersController.js";
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.get("/:id", getUserData);    // GET /user/1234
 router.post("/:id/albums", postAlbum);    // POST /user/1234/albums
 
 router.delete("/:id/albums", deleteAlbums);    // DELETE /user/1234/albums
+
+router.delete("/:id/albums/:albumId", deleteAlbum);  // DELETE /users/1234/albums/5678
 
 export default router;
